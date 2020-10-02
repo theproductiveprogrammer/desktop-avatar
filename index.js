777486a3f58ea09c528cb426e15bab4df1a87d2f
Charles L <charles.lobo@gmail.com>
2020-10-02 16:28:41 +1000

    feat: start electron app

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1300,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    backgroundColor: "#0490f9",
+  })
+
+  mainWindow.loadFile("index.html")
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on("activate", () => {
+    if(BrowserWindow.getAllWindows().length == 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if(process.platform != "darwin") app.quit()
+})
