2438b2c3531bb761e5969c303fa17f19020db981
Charles L <charles.lobo@gmail.com>
2020-10-07 13:47:03 +1100

    feat: get list of users from server

--- store.js
+++ store.js
@@ -5,6 +5,8 @@ const store = dux.createStore(reducer, {})
 
 function reducer(state, type, payload) {
   switch(type) {
+    case "set/users":
+      return { ...state, users: payload }
     case "set/settings":
       return { ...state, settings: payload }
     case "set/userinfo":
--- workflow.js
+++ workflow.js
@@ -1,5 +1,6 @@
 'use strict'
 const dh = require('./display-helpers.js')
+const req = require('./req.js')
 
 function start(store, logger) {
   let userinfo
@@ -14,8 +15,78 @@ function start(store, logger) {
 }
 
 function loggedIn(ui, store, logger) {
-  logger.botMsg(`${dh.greeting()} ${dh.userName(ui)}`)
+  logger.botMsg(`${dh.greeting()} <b>${dh.userName(ui)}</b>`)
 
+  store.react("settings.serverURL", serverURL => {
+    serverURL = parse_url_1(serverURL)
+    if(!serverURL) {
+      logger.botMsg(`
+I want to connect to the server but the url in "settings" is invalid. Please correct it by clicking the "settings gear" icon on the top left of this page.
+`)
+      return
+    }
+
+    getUsers(serverURL, ui, store, logger)
+  })
+
+  function parse_url_1(u) {
+    try {
+      u = new URL(store.get("settings").serverURL)
+      if(u.protocol == "http:" || u.protocol == "https:") {
+        return u
+      }
+    } catch(e) {}
+  }
+}
+
+
+function getUsers(serverURL, ui, store, logger) {
+  logger.botMsg("I'm asking the server for a list of users")
+  let options = {
+    protocol: serverURL.protocol,
+    host: serverURL.hostname,
+    port: serverURL.port,
+    path: "/dapp/v2/myusers",
+    method: "POST",
+  }
+  req.send(options, {
+    id: ui.id,
+    seed: ui.seed,
+    authKey: ui.authKey,
+  }, (status, err, resp, headers) => {
+    if(status != 200 && !err) {
+      err = `getUsers: server responded with status: ${status}`
+    }
+    if(err) {
+      logger.err("getUsers failed", err)
+      logger.svrMsg(`Error!`)
+      logger.botMsg(`
+Could not get users from the server. Please check the message log for more details.
+`)
+    } else {
+      logger.svrMsg("Here you go")
+      try {
+        let users = JSON.parse(resp)
+        if(!users || !users.length) {
+          logger.botMsg(`
+You don't have any other users to manage ${dh.userName(ui)}. Let's work on our own tasks!
+`)
+          users = [ui]
+        } else {
+          users = [ui].concat(users)
+        }
+        logger.msg({
+          t: (new Date()).toISOString(),
+
+        })
+        store.event("set/users", users)
+
+      } catch(e) {
+        logger.err("getUsers response error", err)
+        logger.botMsg(`The server's response did not make sense to me. Please check the message log for more details`)
+      }
+    }
+  })
 }
 
 function loggedOut(ui, store, logger) {
