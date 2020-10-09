ee0cf9aa48ea578551b71fe220376af50bba4787
Charles L <charles.lobo@gmail.com>
2020-10-09 22:49:13 +1100

    feat: improve main preloader

--- preload-main.js
+++ preload-main.js
@@ -1,9 +1,14 @@
 'use strict'
 const { ipcRenderer, contextBridge } = require('electron')
 
-ipcRenderer.invoke("get-logname").then(name => {
-  contextBridge.exposeInMainWorld("logname", { name })
-  contextBridge.exposeInMainWorld("show", {
-    settings: () => ipcRenderer.invoke("show-settings")
-  })
+contextBridge.exposeInMainWorld("show", {
+  settings: () => ipcRenderer.invoke("show-settings"),
 })
+contextBridge.exposeInMainWorld("set", {
+  ui : info => ipcRenderer.invoke("set-userinfo", info),
+})
+contextBridge.exposeInMainWorld("get", {
+  logname : () => ipcRenderer.invoke("get-logname"),
+  settings : () => ipcRenderer.invoke("get-settings"),
+})
+
