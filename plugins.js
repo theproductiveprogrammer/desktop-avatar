8e3c846ca6a7e30967437af7b353c20db9effe9e
Charles L <charles.lobo@gmail.com>
2020-10-14 16:57:56 +1100

    feat: add ability to run plugin code

--- plugins.js
+++ plugins.js
@@ -1,6 +1,7 @@
 'use strict'
 const path = require('path')
 const fs = require('fs')
+const vm = require('vm')
 
 const { clone, pull } = require('isomorphic-git')
 const http = require('isomorphic-git/http/node')
@@ -27,7 +28,7 @@ function cloneRepo(from, to, cb) {
     http,
     dir: to,
     url: from,
-  }).then(cb)
+  }).then(() => cb(null, to))
   .catch(cb)
 }
 
@@ -38,10 +39,85 @@ function updateRepo(from, to, cb) {
     dir: to,
     fastForwardOnly: true,
     author: { name: "invalid", email: "in@valid.com" },
-  }).then(cb)
+  }).then(() => cb(null, to))
   .catch(cb)
+}
+
+function getInfo(loc, name, cb) {
+  getPlugin(loc, name, (err, plugin) => {
+    if(err) cb(err)
+    else if(!plugin) return cb("Plugin not found")
+    else if(!plugin.code) return cb("Plugin code not found")
+    else {
+      let context = {
+        plugin: {name, info},
+      }
+      try {
+        vm.createContext(context)
+        plugin.code.runInContext(context)
+        return cb(null, context.plugin)
+      } catch(e) {
+        cb(e)
+      }
+    }
+  })
+}
+
+function getPlugin(loc, name, cb) {
+  getPlugins(loc, (err, plugins) => {
+    if(err) cb(err)
+    else {
+      let plugin = plugins[name]
+      if(!plugin) return cb("No Matching Plugin")
+      loadPlugin(plugin, (err, plugin) => {
+        if(err) cb(err)
+        else cb(null, plugin)
+      })
+    }
+  })
+}
+
+function loadPlugin(plugin, cb) {
+  if(plugin.loaded) return cb(null, plugin)
+  fs.readFile(plugin.p, (err, code) => {
+    if(err) cb(err)
+    else {
+      try {
+        plugin.code = new vm.Script(code)
+        cb(null, plugin)
+      } catch(e) {
+        cb(e)
+      }
+    }
+  })
+}
+
+function getPlugins(loc, cb) {
+  fs.readdir(loc, { withFileTypes: true }, (err, files) => {
+    if(err) cb(err)
+    else {
+      let plugins = {}
+      for(let i = 0;i < files.length;i++) {
+        let f = files[i]
+        if(f.name[0] !== '.'
+            && f.isFile()
+            && f.name.endsWith(".js")) {
+          let n = path.basename(f.name, ".js")
+          let p = path.join(loc, f.name)
+          plugins[n] = { p }
+        }
+      }
+      cb(null, plugins)
+    }
+  })
+
+  function load_1(p) {
+
+  }
 }
 
 module.exports = {
   getLatest,
+  getPlugin,
+  getInfo,
 }
