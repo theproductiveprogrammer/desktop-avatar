127ab83e222f83ddd6f7583b4c08fbb5ca1ce2b4
Charles L <charles.lobo@gmail.com>
2020-11-14 08:22:26 +1100

    feat: don't show title in user name

--- web/display-helpers.js
+++ web/display-helpers.js
@@ -8,12 +8,11 @@ const emoji = require('emojilib')
  */
 function userName(ui) {
   if(!ui) return "(no user)"
-  let r = ui.title ? ui.title + " " : ""
   if(ui.firstName && ui.lastName) {
-    return r + ui.firstName + " " + ui.lastName
+    return ui.firstName + " " + ui.lastName
   }
-  if(ui.firstName) return r + ui.firstName
-  if(ui.lastName) return r + ui.lastName
+  if(ui.firstName) return ui.firstName
+  if(ui.lastName) return ui.lastName
   return "(no name)"
 }
 
