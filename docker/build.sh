8a0b64dd6c420456c948b3cbd0feab6448a802c4
Charles L <charles.lobo@gmail.com>
2021-06-12 15:25:31 +1000

    feat(docker): copy code

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 VERSION=$(cat package.json | grep '^[\t ]*"version"[ \t]*:' | sed 's/.*"version".*"\(.*\)",/\1/')
+
+function copy_code() {
+  cd src
+
+  cp ../../*js .
+
+  rm index.js
+  rm wins.js
+  rm preload-*.js
+  rm dbg.js
+
+  cd ..
+}
+
+copy_code
+exit 
 docker build . -t desktop-avatar:latest
 docker tag desktop-avatar:latest desktop-avatar:$VERSION
 docker tag desktop-avatar:latest everlifeai/desktop-avatar:$VERSION
 docker tag desktop-avatar:latest everlifeai/desktop-avatar:latest
+
+
