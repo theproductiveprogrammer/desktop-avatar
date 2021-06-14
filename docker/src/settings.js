440c17857c9a6ffced0b2986ea322a528aefe4e6
Charles L <charles.lobo@gmail.com>
2021-06-14 13:18:47 +1000

    feat(docker): load settings correctly

--- docker/src/settings.js
+++ docker/src/settings.js
@@ -1,16 +1,20 @@
 'use strict'
+const fs = require('fs')
 const path = require('path')
 
 const loc = require('./loc.js')
 
+/*    way/
+ * load the settings, ensuring that puppetShow is false
+ */
 function load(store, cb) {
-  return cb(1)
   const f = path.join(loc.home(), "settings.json")
   store.event("msg/add", `Loading settings.json...`)
-  fs.readFile(f, (data, err) => {
+  fs.readFile(f, (err, data) => {
     if(err) return cb(err)
     try {
       const settings = JSON.parse(data)
+      if(settings.puppetShow) settings.puppetShow = false
       store.event("settings/set", settings)
       cb()
     } catch(err) {
