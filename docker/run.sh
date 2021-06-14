3556c13db720ca752071402d117db835648613d7
Charles L <charles.lobo@gmail.com>
2021-06-15 00:05:58 +1000

    feat(docker): allow running from anywhere (prod/dev)

--- docker/run.sh
+++ docker/run.sh
@@ -8,11 +8,29 @@ function login() {
   echo
 }
 
+function run_dev() {
+  docker run -it --rm \
+    -v $(pwd)/src:/desktop-avatar/src \
+    -v $(pwd)/desktop-avatar-docker-db:/root/desktop-avatar \
+    -e "SALESBOX_USERNAME=$uname" \
+    -e "SALESBOX_PASSWORD=$passw" \
+    desktop-avatar:latest "$@"
+}
+
+function run_prod() {
+  docker run -it --rm \
+    -v $(pwd)/desktop-avatar-docker-db:/root/desktop-avatar \
+    -e "SALESBOX_USERNAME=$uname" \
+    -e "SALESBOX_PASSWORD=$passw" \
+    desktop-avatar:latest "$@"
+}
+
 login
 
-docker run -it --rm \
-  -v $(pwd)/src:/desktop-avatar/src \
-  -v $(pwd)/desktop-avatar-docker-db:/root/desktop-avatar \
-  -e "SALESBOX_USERNAME=$uname" \
-  -e "SALESBOX_PASSWORD=$passw" \
-  desktop-avatar:latest "$@"
+if [ "$1" == "dev" ]
+then
+  shift
+  run_dev "$@"
+else
+  run_prod
+fi
