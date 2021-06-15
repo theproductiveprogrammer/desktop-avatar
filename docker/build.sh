5c4520bf5eef0953706162c09b464fa9e104da56
Charles L <charles.lobo@gmail.com>
2021-06-15 14:04:02 +1000

    feat(docker): push to charleslobo77 docker hub

--- docker/build.sh
+++ docker/build.sh
@@ -42,8 +42,8 @@ function build_docker() {
 
   docker build . -t desktop-avatar:latest
   docker tag desktop-avatar:latest desktop-avatar:$VERSION
-  docker tag desktop-avatar:latest everlifeai/desktop-avatar:$VERSION
-  docker tag desktop-avatar:latest everlifeai/desktop-avatar:latest
+  docker tag desktop-avatar:latest charleslobo77/desktop-avatar:$VERSION
+  docker tag desktop-avatar:latest charleslobo77/desktop-avatar:latest
 }
 
 
