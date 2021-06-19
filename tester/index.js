3e4e1e7c11f43b31c0028988519bc716919259b5
Charles L <charles.lobo@gmail.com>
2021-06-19 12:33:55 +1000

    feat(tester): dump unhandled request data

--- tester/index.js
+++ tester/index.js
@@ -4,8 +4,12 @@ const app = express()
 
 const port = 5555
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.use('/', (req, res, next) => {
   console.log('UNHANDLED REQUEST:', `${req.originalUrl} ${req.method}`)
+  console.log(req.body)
   next()
 })
 
