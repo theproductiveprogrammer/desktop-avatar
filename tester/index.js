b6e3f27af7eb0f9ff7a26c472005ee956feaf6ca
Charles L <charles.lobo@gmail.com>
2021-06-19 22:01:49 +1000

    feat(tester): log activity

--- tester/index.js
+++ tester/index.js
@@ -12,24 +12,31 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.post('/user/add', (req, res) => {
+  o('adding user', req.body)
   users.add(req.body)
   res.end()
 })
 
 app.post('/dapp/v2/login', (req, res) => {
+  o('logging in', req.body)
   const ui = users.login(req.body)
   if(!ui) res.status(400).end()
   else res.send(ui)
 })
 
-app.post('/dapp/v2/myusers', (req, res) => res.send("[]"))
+app.post('/dapp/v2/myusers', (req, res) => {
+  o('fetching users to manage', req.body)
+  res.send("[]")
+})
 
 app.post('/tasks/add', (req, res) => {
+  o('adding task', req.body)
   tasks.add(req.body)
   res.end()
 })
 
 app.post('/dapp/v2/tasks', (req, res) => {
+  o('getting tasks', req.body)
   const users = req.body.forUsers
   if(!users) res.status(400).end()
   else res.send(tasks.getFor(users.map(u => u.id)))
@@ -40,6 +47,8 @@ app.use('/', (req, res, next) => {
   console.log(req.body)
   next()
 })
+
+function o(msg, data) { console.log(`${msg}: ${JSON.stringify(data)}`) }
 
 babydb.onExitSignal(() => process.exit())
 
