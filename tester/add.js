db3592677b35239900f6a88b0c752930e43efd62
Charles L <charles.lobo@gmail.com>
2021-06-19 17:06:14 +1000

    feat(tester): add task functionality

--- tester/add.js
+++ tester/add.js
@@ -9,6 +9,7 @@ const serverURL = 'http://localhost:5555'
 function main() {
   const fns = {
     user,
+    task,
     help,
     "-h": help,
     "--help": help,
@@ -21,6 +22,7 @@ function main() {
 function help() {
   console.log(`Usage:
   add user name password
+  add task [view|connect|msg] userid linkedin-url/id [field value, field value, ....]
 `)
 }
 
@@ -31,6 +33,40 @@ function user(args) {
   req.post(serverURL + '/user/add', { usr, pwd }, err => {
     if(err) err_(err)
   })
+}
+
+function task(args) {
+  const t = {
+    action: action_1(args[0]),
+    userid: args[1],
+    linkedInURL: ll_url_1(args[2]),
+  }
+  for(let i = 3;i < args.length;i+=2) {
+    const n = args[i]
+    const v = args[i+1]
+    if(n && v) t[n] = v
+  }
+  console.log(`Saving task: ${JSON.stringify(t)}`)
+  req.post(serverURL + '/tasks/add', t, err => {
+    if(err) err_(err)
+  })
+
+  function action_1(a) {
+    const m = {
+      view: "LINKEDIN_VIEW",
+      connect: "LINKEDIN_CONNECT",
+      msg: "LINKEDIN_MSG",
+      disconnect: "LINKEDIN_DISCONNECT",
+      "check-connect": "LINKEDIN_CHECK_CONNECT",
+      "check-msg": "LINKEDIN_CHECK_MSG",
+    }
+    return m[a]
+  }
+
+  function ll_url_1(u) {
+    if(u.startsWith("http")) return u
+    return `https://www.linkedin.com/in/${u}/`
+  }
 }
 
 function err_(m) {
