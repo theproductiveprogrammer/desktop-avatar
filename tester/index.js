c65869cb20c9d78200eba548e5a74adf10cd69ff
Charles L <charles.lobo@gmail.com>
2021-06-19 12:31:18 +1000

    feat(tester): log incoming requests

--- tester/index.js
+++ tester/index.js
@@ -4,4 +4,9 @@ const app = express()
 
 const port = 5555
 
+app.use((req, res, next) => {
+  console.log(`${req.originalUrl} ${req.method}`)
+  next()
+})
+
 app.listen(port, () => console.log(`Listening at port ${port}...`))
