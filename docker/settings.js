a20ba811c31becda969c5cab8cd85821d27c6631
Charles L <charles.lobo@gmail.com>
2021-06-30 08:23:58 +1000

    feat(docker): set up default if no settings available

--- docker/settings.js
+++ docker/settings.js
@@ -11,6 +11,10 @@ function load(store, cb) {
   const f = path.join(loc.home(), "settings.json")
   store.event("msg/add", `Loading settings.json...`)
   fs.readFile(f, (err, data) => {
+    if(err && err.code == 'ENOENT') {
+        store.event("settings/set", defaultSettings())
+        return cb()
+    }
     if(err) return cb(err)
     try {
       const settings = JSON.parse(data)
@@ -21,6 +25,16 @@ function load(store, cb) {
       cb(err)
     }
   })
+}
+
+function defaultSettings() {
+  return {
+    serverURL: "https://app3.salesbox.ai",
+    timeout: 100000,
+    maxbrowsers: 2,
+    userips: [],
+    userList: ""
+  }
 }
 
 module.exports = {
